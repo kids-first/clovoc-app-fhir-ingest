0b970287bb53d275b0f5c20d80cbad3acd7a3015
liberaliscomputing <liberalis.computing@gmail.com>
2022-05-18 22:24:56 -0400

    :sparkles: extract metabolomic files

---
+++ kf_ingest_packages/packages/CLOVoc/extract_configs/vur_metabolome_file_manifest.py
@@ -0,0 +1,36 @@
+"""
+Extract config module for vur - metabolome file manifest.
+
+See documentation at
+https://kids-first.github.io/kf-lib-data-ingest/tutorial/extract.html for
+information on writing extract config files.
+"""
+
+from kf_lib_data_ingest.common import constants
+from kf_lib_data_ingest.common.concept_schema import CONCEPT
+from kf_lib_data_ingest.etl.extract.operations import keep_map, value_map
+
+source_data_url = "file://../data/vesico-ureteric-reflux/MetabolomeFileManifest.tsv"
+
+
+def do_after_read(df):
+    print(df.head())
+    return df
+
+
+operations = [
+    keep_map(in_col="Study ID", out_col=CONCEPT.STUDY.ID),
+    keep_map(in_col="Participant ID", out_col=CONCEPT.PARTICIPANT.ID),
+    keep_map(in_col="Specimen ID", out_col=CONCEPT.BIOSPECIMEN.ID),
+    keep_map(in_col="Specimen ID", out_col=CONCEPT.GENOMIC_FILE.ID),
+    keep_map(in_col="Analyte Type", out_col=CONCEPT.BIOSPECIMEN.ANALYTE),
+    value_map(
+        in_col="Metabolite Data URL",
+        m=lambda x: [x],
+        out_col=CONCEPT.GENOMIC_FILE.URL_LIST,
+    ),
+    keep_map(in_col="Lab", out_col="PRACTITIONER|ADDRESS"),
+    keep_map(in_col="Contact Person", out_col="PRACTITIONER|NAME"),
+    keep_map(in_col="Email", out_col="PRACTITIONER|TELECOM|EMAIL"),
+    keep_map(in_col="Phone", out_col="PRACTITIONER|TELECOM|PHONE"),
+]
